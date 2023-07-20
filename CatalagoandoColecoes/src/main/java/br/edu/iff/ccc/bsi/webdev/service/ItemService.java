cd45948d3e57de3795cbdccad4434f2e779a3724
DouglasFO <douglasfranca2290@gmail.com>
2023-07-20 16:13:58 -0300

    Implementando função para salvar em ItemService

--- CatalagoandoColecoes/src/main/java/br/edu/iff/ccc/bsi/webdev/service/ItemService.java
+++ CatalagoandoColecoes/src/main/java/br/edu/iff/ccc/bsi/webdev/service/ItemService.java
@@ -1,14 +1,37 @@
 package br.edu.iff.ccc.bsi.webdev.service;
 
+import java.util.Objects;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.edu.iff.ccc.bsi.webdev.entities.Hq;
+import br.edu.iff.ccc.bsi.webdev.entities.Item;
+import br.edu.iff.ccc.bsi.webdev.entities.Manga;
+import br.edu.iff.ccc.bsi.webdev.repository.HqRepository;
 import br.edu.iff.ccc.bsi.webdev.repository.ItemRepository;
+import br.edu.iff.ccc.bsi.webdev.repository.MangaRepository;
 
 @Service
 public class ItemService {
 	
 	@Autowired
 	private ItemRepository itemRepository;
+	
+	@Autowired
+	private MangaRepository mangaRepository;
+	
+	@Autowired
+	private HqRepository hqRepository;
+	
+	public boolean save(Item item, Manga manga, Hq hq) {
+		Item i = itemRepository.save(item);
+		if (Objects.isNull(hq)) {
+			Manga m = mangaRepository.save(manga);
+		} else {
+			Hq quadrinho = hqRepository.save(hq);
+		}
+		return true;
+	}
 
 }
