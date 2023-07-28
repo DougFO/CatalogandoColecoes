e45e79d4f14c45e7df8a66f2a575d0f5334157f6
DouglasFO <douglasfranca2290@gmail.com>
2023-07-28 03:41:58 -0300

    Adicionando alterações da tela de Cadastro de Item

--- CatalagoandoColecoes/src/main/java/br/edu/iff/ccc/bsi/webdev/controller/view/CadastroColecaoControllerView.java
+++ CatalagoandoColecoes/src/main/java/br/edu/iff/ccc/bsi/webdev/controller/view/CadastroColecaoControllerView.java
@@ -10,7 +10,7 @@ public class CadastroColecaoControllerView {
 	
 	@GetMapping
 	public String page() {
-		return "/cadastro/cadastro_colecao.html";
+		return "cadastro/cadastro_colecao.html";
 	}
 
 }
