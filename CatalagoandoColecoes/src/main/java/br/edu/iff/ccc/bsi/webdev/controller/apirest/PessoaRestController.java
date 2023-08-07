2b9890694e0396206379f3c7c5236d8942f6fe28
DouglasFO <douglasfranca2290@gmail.com>
2023-08-07 02:30:59 -0300

    Criada API Rest de Pessoa e copiado do MainRest a operação save (cadastrar)

---
+++ CatalagoandoColecoes/src/main/java/br/edu/iff/ccc/bsi/webdev/controller/apirest/PessoaRestController.java
@@ -0,0 +1,30 @@
+package br.edu.iff.ccc.bsi.webdev.controller.apirest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.iff.ccc.bsi.webdev.entities.Endereco;
+import br.edu.iff.ccc.bsi.webdev.entities.Pessoa;
+import br.edu.iff.ccc.bsi.webdev.entities.Usuario;
+import br.edu.iff.ccc.bsi.webdev.service.PessoaService;
+
+@RestController
+@RequestMapping(path = "/pessoa")
+public class PessoaRestController {
+	
+	@Autowired
+	PessoaService pessoaService;
+	
+	@PostMapping
+	public String save(Pessoa pessoa, Usuario user, Endereco endereco) {
+		
+		if(pessoaService.save(user, pessoa, endereco)) {
+			return "Usuário adicionado! ";
+		} else {
+			return "Usuário não adicionado! ";
+		}
+	}
+
+}
